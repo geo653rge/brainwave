9d7ddec28c1f1e62b358437d9480ed1a12f6af47
geo653rge <georgekimanikiringa@gmail.com>
2024-09-04 16:29:52 +0300

    adding reusable components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from '../src/assets/svg/ButtonGradient.jsx'
+import Button from './Components/Button.jsx';
+
+const App =() =>  {
+  return (
+    <> 
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className="mt-10">
+          Something
+        </Button>
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
