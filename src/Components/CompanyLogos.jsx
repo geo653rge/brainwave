9d2cb1029fbe7b5616a49efe8763b773fd193ca1
geo653rge <georgekimanikiringa@gmail.com>
2024-11-04 17:09:19 +0300

    adding a company Logo section

---
+++ src/Components/CompanyLogos.jsx
@@ -0,0 +1,23 @@
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="mb-6 text-center tagline">
+        Helping people create beautiful content at
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+          >
+            <img src={logo} width={134} height={28} alt={logo} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CompanyLogos;
